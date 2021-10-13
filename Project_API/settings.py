57fbe290961a632289ec4756cee047e3fba8f4c9
Mafe Toro <mtoro0926@gmail.com>
2021-10-13 10:45:45 -0500

    changes

--- Project_API/settings.py
+++ Project_API/settings.py
@@ -54,11 +54,14 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost',
-    'http://localhost:3000',
-    'http://localhost:4200',
-)
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3030',
+] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'http://localhost:3030',
+]
 
 ROOT_URLCONF = 'Project_API.urls'
 
